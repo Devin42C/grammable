92e7b682c3ff87002f60482c7fb698d027045e83
Devin Crossman <devin.crossman2@gmail.com>
2019-10-20 21:47:05 +0000

    Finished Styling

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,10 +1,55 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  
+  def destroy 
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    if @gram.user != current_user
+      return render plain: 'Forbidden', status: :Forbidden
+    end
+    @gram.destroy
+    redirect_to root_path
+  end
+
   def new
     @gram = Gram.new
   end
 
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+
+    if @gram.user != current_user
+      return render plain: 'Forbidden :(', status: :Forbidden
+    end
+
+    @gram.update_attributes(gram_params)
+    
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   def index
+    @grams = Gram.all
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    if @gram.user != current_user
+      render plain: 'Forbidden :(', status: :Forbidden
+    end
   end
 
   def create 
@@ -19,7 +64,6 @@ class GramsController < ApplicationController
   private 
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
-
 end
